972ec7fd41c0ed23f61d39487d3ab5e5c7178794
Unknown <a8c45b30@opayq.com>
2017-07-06 12:27:35 +0200

    Chomp the \n
    chomp ($text = <STDIN>); in een keer tekst lezen zonder enter

---
+++ Chomp Easy.pl
@@ -0,0 +1,11 @@
+#! /usr/perl/
+use warnings;
+
+
+
+chomp($text = <STDIN>); #Read the text, without the newline character
+
+$text = <STDIN>; #Do the same thing
+chomp($text); #...but in two steps
+
+
--- String_re_re_repetition.pl
+++ String_re_re_repetition.pl
@@ -6,5 +6,5 @@ my $name= 'fred';
 my $other= 'BARNERY';
 
 print "$name\n" x 5;
-print "$name" x 10;
 print "$other" x 3;
+print "$name" x 10;
