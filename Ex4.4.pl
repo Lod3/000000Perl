d104f1a80de2ae158ee46a88432eae6aa2358c8a
Unknown <a8c45b30@opayq.com>
2017-08-07 14:13:24 +0200

    Exercise and Start Of morse Code translator
    Morse code translator example found. Storing for later reference. Chapter 5

---
+++ Ex4.4.pl
@@ -0,0 +1,23 @@
+#!/usr/perl
+use warnings;
+use strict;
+
+use 5.010;
+
+greet( 'Fred' );
+greet( 'Barney' );
+greet (' nastasia');
+
+sub greet {
+state $last_person;
+my $name = shift;
+print "Hi $name! ";
+if( defined $last_person ) {
+print "$last_person is also here!\n";
+}
+else {
+print "You are the first one here!\n";
+}
+$last_person = $name;
+}
+
--- User Input.pl
+++ User Input.pl
@@ -1,13 +1,10 @@
 #! /usr/perl
+use warnings;
+
 
 $line = <STDIN>;
 if ($line eq "\n") {
     print "that was just a blank line!";
-    while ($line eq '
-    
-    
-} else {
-    print "that line of input was: $line";
-
+} else { print " That line of input was: $line";
 }
 
