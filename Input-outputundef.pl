3fe32c7cdf73aea0bb612e36ebd501a920e64ddc
Unknown <a8c45b30@opayq.com>
2017-09-21 09:00:45 +0200

    Input Output
    Diamon operator double diamond operator

--- Input-outputundef.pl
+++ Input-outputundef.pl
@@ -3,6 +3,10 @@ use warnings;
 # use strict;
 
 
+# while (<STDIN>) {
+#     print "I saw $_";
+# }
+
 # while (defined($lin = <STDIN>)) {  
 #     print "I saw $lin";
 # }
@@ -11,9 +15,33 @@ use warnings;
 #     print "I saw $_"
 # }
 
-while (defined($line = <>)){
-    chomp($line);
-    print "it was $line that I saw! in $ARGV\n";
+# while (defined($line = <>)){
+#     chomp($line);
+#     print "it was $line that I saw! in $ARGV\n";
     
-}
+# }
 
+# while (<>) {
+#     chomp;
+#     print "I saw $_ in $ARGV\n";
+# }
+
+
+# use v5.22;
+
+# while (<<>>){
+#     chomp;
+#     print "It was $_ that I saw!\n";
+# }
+
+# @ARGV = qw# fred.txt barney.txt Ex4.4.pl #; #force these files to be read. When 
+# while (<>) {
+#     chomp;
+#     print "It was $_ that I saw in some flinstone-like file!\n";
+# }
+
+@ARGV = qw#/home/lode/Documents/packed.ovpn#; #Directory to file to read. 
+while (<>) {                                  #
+    chomp;
+    print "$_";                               #Print only input file content stored in $_ 
+}
---
+++ output to standard output.pl
@@ -0,0 +1,11 @@
+#!/usr/perl
+use warnings;
+use strict;
+
+my $name = "Larry Wall";
+print "hello there, $name, did you know that 3+4 is ", 3+4, "?\n"; 
+
+my @array = qw/ fred barney betty /;
+
+print "@array";
+
